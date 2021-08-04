b87debf3cbfc134d2ad0d9c7a6011b8ae5d039cf
leeo2020 <3145875098@qq.com>
2021-08-05 01:42:37 +0800

    添加二级菜单组件

---
+++ src/components/good/GoodItem.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="lx-goodItem">
+      <van-img src="//img12.360buyimg.com/mobilecms/s372x372_jfs/t1/125129/23/15666/585032/5f90128aE7954e7a4/4bdd982507698f0c.jpg!q70.dpg.webp"></van-img>
+      div
+  </div>
+</template>
+
+<script>
+import {Grid, GridItem, Image} from 'vant'
+export default {
+  name: "GoodCate",
+  components:{
+    [Grid.name]:Grid,
+    [GridItem.name]:GridItem,
+    [Image.name]: Image
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
